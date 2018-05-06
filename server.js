a41071ba47566ee0ba424eeecb8388ec08f99117
Patrick East <patrick.east@icloud.com>
2018-05-06 11:36:35 -0600

    Hello.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const survey = require('./app/public/home.html')
+// const survey = require('./app/public/home.html')
 
 
 
