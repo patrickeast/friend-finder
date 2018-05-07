a434c124f54cb606ab07c12432f7d63419a2bd73
Patrick East <patrick.east@icloud.com>
2018-05-06 20:44:46 -0600

    Final Commit.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,12 +4,16 @@ module.exports = function(app) {
   app.get('/', function (req, res) {
       res.sendFile(path.join(__dirname, '/../public/home.html'));
   });
-  
+
   app.get('/survey', function (req, res) {
       res.sendFile(path.join(__dirname, '/../public/survey.html'));
   });
 
   app.use(function (req, res) {
       res.sendFile(path.join(__dirname + '/../public/home.html'));
+  });
+  
+  app.use(function (req, res) {
+      res.sendFile(path.join(__dirname + '/../public/success.html'));
   });
 };
